8dd6f690fc2bbe89158ecfa5abe9a73c5d0c6e5c
retterrudi <paul.serdack+retterrudi@gmail.com>
2024-11-25 08:24:07 +0100

    Cleanup

--- CSVParser/src/CSVParser/Table.cs
+++ CSVParser/src/CSVParser/Table.cs
@@ -40,31 +40,3 @@ public class Table(
     }
 
 }
-
-
-public static class HtmlConverter
-{
-    // TODO: Tests
-    internal static string FillTemplate(
-        Table table, 
-        string fileTemplate, 
-        string rowsTemplate)
-    {
-        var rows = "";
-        
-        foreach (var row in table.rows)
-        {
-            var filledRowsTemplate = rowsTemplate;
-            foreach (var column in table.header)
-            {
-                filledRowsTemplate = 
-                    filledRowsTemplate.Replace($"{{{{{column}}}}}", row[column]);
-            }
-
-            rows += filledRowsTemplate;
-        }
-
-        return fileTemplate.Replace("{{rows}}", rows);
-    }
-    
-}
